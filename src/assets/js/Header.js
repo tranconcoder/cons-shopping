944be59d613c348a60dafb4758400b3152f7fff9
tranconcoder <tranconcoder@gmail.com>
2023-05-23 20:40:23 +0700

    complete validate registerForm on client

--- src/assets/js/Header.js
+++ src/assets/js/Header.js
@@ -5,8 +5,11 @@ class ValidateForm {
             required: "Không được để trống trường này!",
             minLength: (n) => "Độ dài tối thiểu từ " + n + " ký tự!",
             maxLength: (n) => "Độ dài tối đa là " + n + " ký tự!",
+            equalLength: (n) => "Độ dài phải là " + n + "!",
             secureLevel1: "Phải bao gồm số, ký tự hoa, thường và ký tự đặc biệt",
+            numberOnly: "Chỉ cho phép nhập số!",
             notSymbolCheck: "Không nhập ký tự đặc biệt vào trường này",
+            isEmail: "Không đúng định dạng Email, vui lòng nhập lại!",
             requiredSymbol: "Trường này bắt buộc chứa ký tự đặc biệt",
             requiredUpper: "Trường này bắt buộc chứa ký tự in ho",
             requiredLower: "Trường này bắt buộc chứa ký tự in thường",
@@ -19,11 +22,13 @@ class ValidateForm {
         else
             return false;
     }
-    lengthCheck(value, type = "min", range) {
+    lengthCheck(value, type, range) {
         if (type === "min" && value.length < range)
             return this.notify.minLength(range);
         if (type === "max" && value.length > range)
             return this.notify.maxLength(range);
+        if (type === "equal" && value.length !== range)
+            return this.notify.equalLength(range);
         return false;
     }
     notSymbolCheck(value) {
@@ -45,6 +50,16 @@ class ValidateForm {
     requiredNumber(value) {
         const regex_number = /[0-9]/;
         return regex_number.test(value) ? false : this.notify.requiredNumber;
+    }
+    numberOnly(value) {
+        const regex_number_only = /^[0-9]*$/;
+        return regex_number_only.test(value) ? false : this.notify.numberOnly;
+    }
+    isEmail(value) {
+        const regex_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex_email.test(value.toLowerCase())
+            ? false
+            : this.notify.isEmail;
     }
     // Min length: 8
     // Have: symbol, number, uppercase, lowercase
@@ -85,17 +100,17 @@ class ValidateForm {
             if (requiredCheckResult)
                 return this.setError(inputElm, requiredCheckResult);
         }
-        // Min length check
-        if (config.minLength) {
-            const minLengthCheckResult = this.lengthCheck(inputValue, "min", config.minLength);
-            if (minLengthCheckResult)
-                return this.setError(inputElm, minLengthCheckResult);
+        // isGmail check
+        if (config.isEmail) {
+            const isGmailCheckResult = this.isEmail(inputValue);
+            if (isGmailCheckResult)
+                return this.setError(inputElm, isGmailCheckResult);
         }
-        // Max length check
-        if (config.maxLength) {
-            const maxLengthCheckResult = this.lengthCheck(inputValue, "max", config.maxLength);
-            if (maxLengthCheckResult)
-                return this.setError(inputElm, maxLengthCheckResult);
+        if (config.numberOnly) {
+            // Number only check
+            const numberOnlyCheckResult = this.numberOnly(inputValue);
+            if (numberOnlyCheckResult)
+                return this.setError(inputElm, numberOnlyCheckResult);
         }
         // Not symbol check
         if (config.notSymbol) {
@@ -109,8 +124,31 @@ class ValidateForm {
             if (secureLevel1CheckResult)
                 return this.setError(inputElm, secureLevel1CheckResult);
         }
+        // Equal length check
+        if (config.equalLength) {
+            const equalLengthCheckResult = this.lengthCheck(inputValue, "equal", config.equalLength);
+            if (equalLengthCheckResult)
+                return this.setError(inputElm, equalLengthCheckResult);
+        }
+        // Min length check
+        if (config.minLength) {
+            const minLengthCheckResult = this.lengthCheck(inputValue, "min", config.minLength);
+            if (minLengthCheckResult)
+                return this.setError(inputElm, minLengthCheckResult);
+        }
+        // Max length check
+        if (config.maxLength) {
+            const maxLengthCheckResult = this.lengthCheck(inputValue, "max", config.maxLength);
+            if (maxLengthCheckResult)
+                return this.setError(inputElm, maxLengthCheckResult);
+        }
         this.removeError(inputElm);
         return true;
+    }
+    listenAndValidateInput(inputElm, config) {
+        inputElm.addEventListener("focusout", (e) => {
+            this.checkInputValues(inputElm, config);
+        });
     }
 }
 class LoginForm extends ValidateForm {
@@ -121,29 +159,22 @@ class LoginForm extends ValidateForm {
         this.passwordInput = $("#password-input");
     }
     listenEvent() {
-        this.usernameInput.addEventListener("focusout", (e) => {
-            const inputElm = e.target;
-            this.checkInputValues(inputElm, {
-                minLength: 6,
-                maxLength: 24,
-                notSymbol: true,
-            });
-        });
-        this.passwordInput.addEventListener("focusout", (e) => {
-            const inputElm = e.target;
-            this.checkInputValues(inputElm, {
-                secureLevel1: true,
-            });
-        });
+        // Validate config
+        const usernameConfig = {
+            minLength: 6,
+            maxLength: 24,
+            notSymbol: true,
+        };
+        const passwordConfig = {
+            secureLevel1: true,
+        };
+        // Listen focusout event and validate username input
+        this.listenAndValidateInput(this.usernameInput, usernameConfig);
+        // Password input
+        this.listenAndValidateInput(this.passwordInput, passwordConfig);
         this.formElm.addEventListener("submit", (e) => {
-            const usernameCheckResult = this.checkInputValues(this.usernameInput, {
-                minLength: 6,
-                maxLength: 24,
-                notSymbol: true,
-            });
-            const passwordCheckResult = this.checkInputValues(this.passwordInput, {
-                secureLevel1: true,
-            });
+            const usernameCheckResult = this.checkInputValues(this.usernameInput, usernameConfig);
+            const passwordCheckResult = this.checkInputValues(this.passwordInput, passwordConfig);
             const allowSubmit = usernameCheckResult && passwordCheckResult;
             if (!allowSubmit)
                 e.preventDefault();
@@ -160,22 +191,57 @@ class RegisterForm extends ValidateForm {
         this.phoneNumberInput = $("#phone-number-input");
         this.gmailInput = $("#gmail-input");
     }
-    listenEvent() { }
+    listenEvent() {
+        // Validate config
+        const fistNameConfig = {
+            required: true,
+            minLength: 2,
+            maxLength: 8,
+            notSymbol: true,
+        };
+        const lastNameConfig = {
+            required: true,
+            minLength: 1,
+            maxLength: 8,
+            notSymbol: true,
+        };
+        const addressConfig = {
+            required: true,
+            minLength: 10,
+            maxLength: 120,
+        };
+        const phoneNumberConfig = {
+            required: true,
+            numberOnly: true,
+            equalLength: 10,
+        };
+        const gmailConfig = {
+            required: true,
+            isEmail: true,
+        };
+        // Listen and validate input on focusout
+        this.listenAndValidateInput(this.firstNameInput, fistNameConfig);
+        this.listenAndValidateInput(this.lastNameInput, lastNameConfig);
+        this.listenAndValidateInput(this.addressInput, addressConfig);
+        this.listenAndValidateInput(this.phoneNumberInput, phoneNumberConfig);
+        this.listenAndValidateInput(this.gmailInput, gmailConfig);
+    }
 }
 class AuthenticateBox {
     constructor() {
+        this.loginForm = new LoginForm();
+        this.registerForm = new RegisterForm();
         this.checkboxToShowBox = $("#toggle-register-auth-box");
-        this.loginForm = $(".authenticate-box .form-ctn.login");
-        this.registerForm = $(".authenticate-box .form-ctn.register");
+    }
+    listenChangeFormInput() {
+        this.loginForm.listenEvent();
+        this.registerForm.listenEvent();
     }
     listenEventToShowAndHideBox() {
-        this.checkboxToShowBox.addEventListener("change", () => {
-            this.loginForm.reset();
-            this.registerForm.reset();
-        });
+        this.checkboxToShowBox.addEventListener("change", () => { });
     }
 }
-const loginForm = new LoginForm();
-loginForm.listenEvent();
 const authenticateBox = new AuthenticateBox();
+authenticateBox.listenChangeFormInput();
+authenticateBox.listenEventToShowAndHideBox();
 authenticateBox.listenEventToShowAndHideBox();
--- src/assets/ts/Header.ts
+++ src/assets/ts/Header.ts
@@ -1,10 +1,24 @@
+type ValidateFormConfig = Partial<{
+	required: boolean;
+	equalLength: number;
+	minLength: number;
+	maxLength: number;
+	numberOnly: boolean;
+	notSymbol: boolean;
+	secureLevel1: boolean;
+	isEmail: boolean;
+}>;
+
 class ValidateForm {
 	protected readonly notify = {
 		required: "Không được để trống trường này!",
 		minLength: (n: number) => "Độ dài tối thiểu từ " + n + " ký tự!",
 		maxLength: (n: number) => "Độ dài tối đa là " + n + " ký tự!",
+		equalLength: (n: number) => "Độ dài phải là " + n + "!",
 		secureLevel1: "Phải bao gồm số, ký tự hoa, thường và ký tự đặc biệt",
+		numberOnly: "Chỉ cho phép nhập số!",
 		notSymbolCheck: "Không nhập ký tự đặc biệt vào trường này",
+		isEmail: "Không đúng định dạng Email, vui lòng nhập lại!",
 		requiredSymbol: "Trường này bắt buộc chứa ký tự đặc biệt",
 		requiredUpper: "Trường này bắt buộc chứa ký tự in ho",
 		requiredLower: "Trường này bắt buộc chứa ký tự in thường",
@@ -13,47 +27,66 @@ class ValidateForm {
 
 	constructor() {}
 
-	public required(value: String) {
+	private required(value: String) {
 		if (value.length == 0) return this.notify.required;
 		else return false;
 	}
 
-	public lengthCheck(value: string, type = "min", range: number) {
+	private lengthCheck(
+		value: string,
+		type: "min" | "max" | "equal",
+		range: number
+	) {
 		if (type === "min" && value.length < range)
 			return this.notify.minLength(range);
 		if (type === "max" && value.length > range)
 			return this.notify.maxLength(range);
+		if (type === "equal" && value.length !== range)
+			return this.notify.equalLength(range);
 		return false;
 	}
 
-	public notSymbolCheck(value: string) {
+	private notSymbolCheck(value: string) {
 		const regex = new RegExp(/^[A-Za-z0-9]*$/);
 		return regex.test(value) ? false : this.notify.notSymbolCheck;
 	}
 
-	public requiredSymbol(value: string) {
+	private requiredSymbol(value: string) {
 		const regex_symbols = /[-!$%^&*()_+|~=`{}\[\]:\/;<>?,.@#]/;
 		return regex_symbols.test(value) ? false : this.notify.requiredSymbol;
 	}
 
-	public requiredLower(value: string) {
+	private requiredLower(value: string) {
 		const regex_lower = /[a-z]/;
 		return regex_lower.test(value) ? false : this.notify.requiredLower;
 	}
 
-	public requiredUpper(value: string) {
+	private requiredUpper(value: string) {
 		const regex_upper = /[A-Z]/;
 		return regex_upper.test(value) ? false : this.notify.requiredUpper;
 	}
 
-	public requiredNumber(value: string) {
+	private requiredNumber(value: string) {
 		const regex_number = /[0-9]/;
 		return regex_number.test(value) ? false : this.notify.requiredNumber;
 	}
 
+	private numberOnly(value: string) {
+		const regex_number_only = /^[0-9]*$/;
+		return regex_number_only.test(value) ? false : this.notify.numberOnly;
+	}
+
+	private isEmail(value: string) {
+		const regex_email =
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return regex_email.test(value.toLowerCase())
+			? false
+			: this.notify.isEmail;
+	}
+
 	// Min length: 8
 	// Have: symbol, number, uppercase, lowercase
-	public secureLevel1(value: string) {
+	private secureLevel1(value: string) {
 		const lengthCheckResult = this.lengthCheck(value, "min", 8);
 		if (lengthCheckResult) return lengthCheckResult;
 
@@ -83,7 +116,7 @@ class ValidateForm {
 		return false;
 	}
 
-	public removeError(inputElm: HTMLInputElement) {
+	private removeError(inputElm: HTMLInputElement) {
 		const parent = inputElm.parentElement?.parentElement as HTMLDivElement;
 		const errorMessageElm = parent.querySelector(
 			".error-message"
@@ -93,15 +126,9 @@ class ValidateForm {
 		parent.classList.remove("error");
 	}
 
-	public checkInputValues(
+	protected checkInputValues(
 		inputElm: HTMLInputElement,
-		config: Partial<{
-			required: boolean;
-			minLength: number;
-			maxLength: number;
-			notSymbol: boolean;
-			secureLevel1: boolean;
-		}>
+		config: ValidateFormConfig
 	) {
 		const inputValue = inputElm.value;
 
@@ -110,6 +137,45 @@ class ValidateForm {
 			const requiredCheckResult = this.required(inputValue);
 			if (requiredCheckResult)
 				return this.setError(inputElm, requiredCheckResult);
+		}
+
+		// isGmail check
+		if (config.isEmail) {
+			const isGmailCheckResult = this.isEmail(inputValue);
+			if (isGmailCheckResult)
+				return this.setError(inputElm, isGmailCheckResult);
+		}
+
+		if (config.numberOnly) {
+			// Number only check
+			const numberOnlyCheckResult = this.numberOnly(inputValue);
+			if (numberOnlyCheckResult)
+				return this.setError(inputElm, numberOnlyCheckResult);
+		}
+
+		// Not symbol check
+		if (config.notSymbol) {
+			const notSymbolCheckResult = this.notSymbolCheck(inputValue);
+			if (notSymbolCheckResult)
+				return this.setError(inputElm, notSymbolCheckResult);
+		}
+
+		// Secure level 1 for password
+		if (config.secureLevel1) {
+			const secureLevel1CheckResult = this.secureLevel1(inputValue);
+			if (secureLevel1CheckResult)
+				return this.setError(inputElm, secureLevel1CheckResult);
+		}
+
+		// Equal length check
+		if (config.equalLength) {
+			const equalLengthCheckResult = this.lengthCheck(
+				inputValue,
+				"equal",
+				config.equalLength
+			);
+			if (equalLengthCheckResult)
+				return this.setError(inputElm, equalLengthCheckResult);
 		}
 
 		// Min length check
@@ -135,22 +201,17 @@ class ValidateForm {
 				return this.setError(inputElm, maxLengthCheckResult);
 		}
 
-		// Not symbol check
-		if (config.notSymbol) {
-			const notSymbolCheckResult = this.notSymbolCheck(inputValue);
-			if (notSymbolCheckResult)
-				return this.setError(inputElm, notSymbolCheckResult);
-		}
-
-		// Secure level 1 for password
-		if (config.secureLevel1) {
-			const secureLevel1CheckResult = this.secureLevel1(inputValue);
-			if (secureLevel1CheckResult)
-				return this.setError(inputElm, secureLevel1CheckResult);
-		}
-
 		this.removeError(inputElm);
 		return true;
+	}
+
+	protected listenAndValidateInput(
+		inputElm: HTMLInputElement,
+		config: ValidateFormConfig
+	) {
+		inputElm.addEventListener("focusout", (e) => {
+			this.checkInputValues(inputElm, config);
+		});
 	}
 }
 
@@ -168,36 +229,30 @@ class LoginForm extends ValidateForm {
 	}
 
 	public listenEvent() {
-		this.usernameInput.addEventListener("focusout", (e) => {
-			const inputElm = e.target as HTMLInputElement;
-			this.checkInputValues(inputElm, {
-				minLength: 6,
-				maxLength: 24,
-				notSymbol: true,
-			});
-		});
+		// Validate config
+		const usernameConfig = {
+			minLength: 6,
+			maxLength: 24,
+			notSymbol: true,
+		};
+		const passwordConfig = {
+			secureLevel1: true,
+		};
 
-		this.passwordInput.addEventListener("focusout", (e) => {
-			const inputElm = e.target as HTMLInputElement;
-			this.checkInputValues(inputElm, {
-				secureLevel1: true,
-			});
-		});
+		// Listen focusout event and validate username input
+		this.listenAndValidateInput(this.usernameInput, usernameConfig);
+
+		// Password input
+		this.listenAndValidateInput(this.passwordInput, passwordConfig);
 
 		this.formElm.addEventListener("submit", (e) => {
 			const usernameCheckResult = this.checkInputValues(
 				this.usernameInput,
-				{
-					minLength: 6,
-					maxLength: 24,
-					notSymbol: true,
-				}
+				usernameConfig
 			);
 			const passwordCheckResult = this.checkInputValues(
 				this.passwordInput,
-				{
-					secureLevel1: true,
-				}
+				passwordConfig
 			);
 
 			const allowSubmit = usernameCheckResult && passwordCheckResult;
@@ -226,30 +281,67 @@ class RegisterForm extends ValidateForm {
 		this.gmailInput = $("#gmail-input");
 	}
 
-	public listenEvent() {}
+	public listenEvent() {
+		// Validate config
+		const fistNameConfig = {
+			required: true,
+			minLength: 2,
+			maxLength: 8,
+			notSymbol: true,
+		};
+		const lastNameConfig = {
+			required: true,
+			minLength: 1,
+			maxLength: 8,
+			notSymbol: true,
+		};
+		const addressConfig = {
+			required: true,
+			minLength: 10,
+			maxLength: 120,
+		};
+		const phoneNumberConfig = {
+			required: true,
+			numberOnly: true,
+			equalLength: 10,
+		};
+		const gmailConfig = {
+			required: true,
+			isEmail: true,
+		};
+
+		// Listen and validate input on focusout
+		this.listenAndValidateInput(this.firstNameInput, fistNameConfig);
+		this.listenAndValidateInput(this.lastNameInput, lastNameConfig);
+		this.listenAndValidateInput(this.addressInput, addressConfig);
+		this.listenAndValidateInput(this.phoneNumberInput, phoneNumberConfig);
+		this.listenAndValidateInput(this.gmailInput, gmailConfig);
+	}
 }
 
 class AuthenticateBox {
+	private loginForm: LoginForm;
+	private registerForm: RegisterForm;
+
 	private checkboxToShowBox: HTMLInputElement;
-	private loginForm: HTMLFormElement;
-	private registerForm: HTMLFormElement;
 
 	constructor() {
+		this.loginForm = new LoginForm();
+		this.registerForm = new RegisterForm();
 		this.checkboxToShowBox = $("#toggle-register-auth-box");
-		this.loginForm = $(".authenticate-box .form-ctn.login");
-		this.registerForm = $(".authenticate-box .form-ctn.register");
+	}
+
+	public listenChangeFormInput() {
+		this.loginForm.listenEvent();
+		this.registerForm.listenEvent();
 	}
 
 	public listenEventToShowAndHideBox() {
-		this.checkboxToShowBox.addEventListener("change", () => {
-			this.loginForm.reset();
-			this.registerForm.reset();
-		});
+		this.checkboxToShowBox.addEventListener("change", () => {});
 	}
 }
 
-const loginForm = new LoginForm();
-loginForm.listenEvent();
-
 const authenticateBox = new AuthenticateBox();
+authenticateBox.listenChangeFormInput();
+authenticateBox.listenEventToShowAndHideBox();
 authenticateBox.listenEventToShowAndHideBox();
