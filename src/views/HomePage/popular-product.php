1c62cd6dba0c60bd096e00c41e3029e15aa3d49c
tranconcoder <tranconcoder@gmail.com>
2023-06-08 22:11:50 +0700

    use aspect-radio instead of position for popular phone item

--- src/views/HomePage/popular-product.php
+++ src/views/HomePage/popular-product.php
@@ -3,35 +3,33 @@
 
 	<ul class="popular-product-list">
 		<?php for ($i = 0; $i < 8; $i++): ?>
-				<li class="item-wrapper">
-					<div class="item-container">
-						<div class="thumb-ctn">
-							<img src="https://static-images.vnncdn.net/files/publish/2022/8/8/iphone-14-pro-93.jpg" alt="" class="thumb" />
-						</div>
+			<li class="item-container">
+				<div class="thumb-ctn">
+					<img src="https://static-images.vnncdn.net/files/publish/2022/8/8/iphone-14-pro-93.jpg" alt="" class="thumb" />
+				</div>
 
-						<p class="name">Iphone 14 Pro Max 512GB</p>
-						<p class="price">10,990,000 &#8363;</p>
+				<p class="name">Iphone 14 Pro Max 512GB</p>
+				<p class="price">10,990,000 &#8363;</p>
 
-						<div class="detail">
-							<p class="memory">RAM: 6GB</p>
-							<p class="storage">ROM: 128GB</p>
-							<p class="monitor">Màn hình: 2K Retina</p>
-							<p class="battery">Pin: 5000mah</p>
-						</div>
+				<div class="detail">
+					<p class="memory">RAM: 6GB</p>
+					<p class="storage">ROM: 128GB</p>
+					<p class="monitor">Màn hình: 2K Retina</p>
+					<p class="battery">Pin: 5000mah</p>
+				</div>
 
 
-						<div class="button-list">
-							<button class="add-to-cart">
-								<p><i class="fa-solid fa-cart-plus"></i></p>
-							</button>
+				<div class="button-list">
+					<button class="add-to-cart">
+						<p><i class="fa-solid fa-cart-plus"></i></p>
+					</button>
 
-							<button class="buy-now">
-								<p>Mua ngay</p>
-							</button>
-						</div>
-					</div>
-				</li>
-			<?php endfor; ?>
+					<button class="buy-now">
+						<p>Mua ngay</p>
+					</button>
+				</div>
+			</li>
+		<?php endfor; ?>
 	</ul>
 
 	<button class="show-all-product">
